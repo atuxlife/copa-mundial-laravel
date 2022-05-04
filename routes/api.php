e3018e84725f2451a7d9fd739d0e91b05c5f1cd4
Carlos Jacquin Venencia <atuxlife@gmail.com>
2022-05-04 09:41:54 -0500

    Finalización primera versión CRUD equipos

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/equipos', 'App\Http\Controllers\EquipoController@index'); // Listar los equipos
+Route::post('/equipos', 'App\Http\Controllers\EquipoController@store'); // Crear un equipo
+Route::put('/equipos/{id}', 'App\Http\Controllers\EquipoController@update'); // Actualizar un equipo
+Route::delete('/equipos/{id}', 'App\Http\Controllers\EquipoController@destroy'); // Eliminar un equipo
